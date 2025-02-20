1cc5aa47ea521e7d78ddc927b9c6885544b670a9
Jose <joseornella93@gmail.com>
2025-02-20 16:39:08 +0100

    App ready with flask run

--- search.py
+++ search.py
@@ -12,29 +12,28 @@ load_dotenv()
 
 class Search:
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
-
-        print("Intentando conectar con Elasticsearch...")
-        
         while True:  # Bucle infinito hasta que se conecte
             try:
-                        # self.es = Elasticsearch(
+                self.model = SentenceTransformer("all-MiniLM-L6-v2")
+                # self.es = Elasticsearch(
                 #     cloud_id=os.environ["ELASTIC_CLOUD_ID"],
                 #     api_key=os.environ["ELASTIC_API_KEY"],
                 # )
-                #    # hosts=["http://localhost:9200"],
                 self.es = Elasticsearch(
-                    hosts=["http://localhost:9200"],  # Usa el nombre del servicio en Docker elasticsearch
+                    # Si es corre con docker poner:hosts=["http://elasticsearch-goornella:9200"], 
+                    # Si es corro con python -m flask run --host=0.0.0.0 --port=5001 poner hosts=["http://localhost:9200"],
+                    hosts=["http://localhost:9200"], 
                     verify_certs=False
-                )
-                # Intentar obtener información del servidor
+                )  
                 client_info = self.es.info()
-                print("✅ ¡Conectado a Elasticsearch!")
-                print(client_info.body)  # Imprimir detalles de la conexión
+                print("Connected to Elasticsearch!")
+                pprint(client_info.body)
                 break  # Salir del bucle si la conexión es exitosa
+
             except Exception as e:
                 print("❌ Elasticsearch no disponible, reintentando en 5 segundos...")
                 time.sleep(5)  # Esperar 5 segundos antes de intentar de nuevo
+
 
 
     def create_index(self, index_name="my_documents"):
@@ -82,7 +81,7 @@ class Search:
         response = self.es.bulk(operations=operations)
         
         # Convierte la respuesta a un diccionario antes de imprimir
-        print("Bulk Insert Response:", response.body)  
+        #print("Bulk Insert Response:", response.body)  
         return response
 
 
